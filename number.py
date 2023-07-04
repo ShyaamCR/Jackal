6e7de6bd7a3ca2de96a5651da1927516046fdc99
ShyaamCR <shyaam611@gmail.com>
2023-07-04 22:56:19 +0200

    debug

--- number.py
+++ number.py
@@ -1,14 +1,19 @@
 i =1
 x = 1
 
+print('Hi')
 
 for x in range(1,10000):
+    print('x inside')
     for i in range(1,11):
+        print('y inside')
         if x/i == 0:
+            print('if insdie')
             i=+1
             print(x)
             continue
         else:
             x=+1
         
+
 
