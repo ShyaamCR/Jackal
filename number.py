8b98942af98eaf770affc19a9dc422a4c21bce32
ShyaamCR <shyaam611@gmail.com>
2023-07-04 23:00:19 +0200

    updated the logic from / to %

--- number.py
+++ number.py
@@ -4,16 +4,13 @@ x = 1
 print('Hi')
 
 for x in range(1,10000):
-    print('x inside')
     for i in range(1,11):
-        print('y inside')
-        if x/i == 0:
-            print('if insdie')
+        if x%i == 0:
             i=+1
-            print(x)
-            continue
         else:
             x=+1
+    
+print(x)
         
 
 
