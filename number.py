4a5d663d5cf8e29af516bcb612567747ab9fae20
ShyaamCR <shyaam611@gmail.com>
2023-07-05 20:30:59 +0200

    test

--- number.py
+++ number.py
@@ -1,16 +1,15 @@
-i =1
-x = 1
 
-print('Hi')
+# for x in range(1,10000):
+#     for i in range(1,11):
+#         if x%i == 0:
+#             i=+1
+#             continue
+#         else:
+#             x=+1
 
-for x in range(1,10000):
-    for i in range(1,11):
-        if x%i == 0:
-            i=+1
-        else:
-            x=+1
+# print(x)
     
-print(x)
-        
+
+print(3/5)    
 
 
